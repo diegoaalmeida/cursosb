140e790834f0feee990cb5e76985a60b91791786
Diego Almeida <diegoangelo.almeida@gmail.com>
2020-07-10 00:13:27 -0300

    Ajustes no endpont

--- src/main/java/com/diegoalmeida/cursosb/resources/CategoriaResource.java
+++ src/main/java/com/diegoalmeida/cursosb/resources/CategoriaResource.java
@@ -18,10 +18,9 @@ public class CategoriaResource {
 	private CategoriaService service;
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Categoria obj = service.buscar(id);
-		
-		return ResponseEntity.ok(obj);
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
+		Categoria obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
--- src/main/java/com/diegoalmeida/cursosb/services/CategoriaService.java
+++ src/main/java/com/diegoalmeida/cursosb/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.diegoalmeida.cursosb.domain.Categoria;
 import com.diegoalmeida.cursosb.repositories.CategoriaRepository;
+import com.diegoalmeida.cursosb.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,9 +15,10 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
