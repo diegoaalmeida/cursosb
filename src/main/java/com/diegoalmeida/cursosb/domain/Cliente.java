b4a43430b1c65b49a7d5b5f2effca5314acec196
Diego Almeida <diegoangelo.almeida@gmail.com>
2020-07-20 00:10:52 -0300

    Salvando perfis de usuário na base de dados

--- src/main/java/com/diegoalmeida/cursosb/domain/Cliente.java
+++ src/main/java/com/diegoalmeida/cursosb/domain/Cliente.java
@@ -5,17 +5,19 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
-import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.diegoalmeida.cursosb.domain.enums.Perfil;
 import com.diegoalmeida.cursosb.domain.enums.TipoCliente;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -46,6 +48,10 @@ private Set<String> telefones = new HashSet<>();
 @OneToMany(mappedBy="cliente")
 private List<Pedido> pedidos = new ArrayList<>();
 
+@ElementCollection(fetch=FetchType.EAGER)
+@CollectionTable(name="PERFIS")
+private Set<Integer> perfis = new HashSet<>();
+
 public Cliente() {
 	
 }
@@ -58,6 +64,7 @@ public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoClie
 	this.cpfOuCnpj = cpfOuCnpj;
 	this.tipo = (tipo==null) ? null : tipo.getCod();
 	this.senha = senha;
+	addPerfil(Perfil.CLIENTE);
 }
 
 public Integer getId() {
@@ -131,6 +138,14 @@ public List<Pedido> getPedidos() {
 
 public void setPedidos(List<Pedido> pedidos) {
 	this.pedidos = pedidos;
+}
+
+public Set<Perfil> getPerfis() {
+	return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+}
+
+public void addPerfil(Perfil perfil) {
+	perfis.add(perfil.getCod());
 }
 
 @Override
