d1ddb130aeb8b0424722cb0acabca0d14c2aabfd
Diego Almeida <diegoangelo.almeida@gmail.com>
2020-07-19 23:02:51 -0300

    Adicionando senha a Cliente

--- src/main/java/com/diegoalmeida/cursosb/domain/Cliente.java
+++ src/main/java/com/diegoalmeida/cursosb/domain/Cliente.java
@@ -32,6 +32,9 @@ private String email;
 private String cpfOuCnpj;
 private Integer tipo;
 
+@JsonIgnore
+private String senha;
+
 @OneToMany(mappedBy="cliente", cascade=CascadeType.ALL)
 private List<Endereco> enderecos = new ArrayList<>();
 
@@ -47,14 +50,14 @@ public Cliente() {
 	
 }
 
-public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
+public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo, String senha) {
 	super();
 	this.id = id;
 	this.nome = nome;
 	this.email = email;
 	this.cpfOuCnpj = cpfOuCnpj;
 	this.tipo = (tipo==null) ? null : tipo.getCod();
-
+	this.senha = senha;
 }
 
 public Integer getId() {
@@ -97,7 +100,13 @@ public void setTipo(TipoCliente tipo) {
 	this.tipo = tipo.getCod();
 }
 
+public String getSenha() {
+	return senha;
+}
 
+public void setSenha(String senha) {
+	this.senha = senha;
+}
 
 public List<Endereco> getEnderecos() {
 	return enderecos;
